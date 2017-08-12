30ecb5a5958f6cd2de4d5f8b47e8223c72309052
Stevenhr <estevenhr@hotmail.com>
2017-08-12 12:21:26 -0500

    Commit pull
    Ejemplo pull

--- src/ejercicioog2/EjerciciooG2.java
+++ src/ejercicioog2/EjerciciooG2.java
@@ -16,7 +16,7 @@ public class EjerciciooG2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo desde git");
+        System.out.println("Hola mundo desde git 2017 pull");
     }
     
 }
